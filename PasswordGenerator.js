eba1ead2eb5eb512def329c2f88e56639aa6db13
Reyhan Huditama <reyhan21098@gmail.com>
2019-01-23 13:29:46 +0700

    Tugas Week 5

---
+++ PasswordGenerator.js
@@ -0,0 +1,84 @@
+function changeVocals (str) {
+  var result = ''
+  
+  for(i = 0; i < str.length; i++) {
+    
+    if(str[i] === 'a'){
+      result += 'b'
+    } else if(str[i] === 'i'){
+      result += 'j'
+    } else if(str[i] === 'u'){
+      result += 'v'
+    } else if(str[i] === 'e'){
+      result += 'f'
+    } else if(str[i] === 'o'){
+      result += 'p'
+    } else if(str[i] === 'A'){
+      result += 'B'
+    } else if(str[i] === 'I'){
+      result += 'J'
+    } else if(str[i] === 'U'){
+      result += 'V'
+    } else if(str[i] === 'e'){
+      result += 'F'
+    } else if(str[i] === 'O'){
+      result += 'P'
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
+  
+function reverseWord (str) {
+    var result = ''
+
+    for (i = str.length - 1; i >= 0; i--) {
+      result += str[i]
+    }
+  return result  
+}
+  
+function setLowerUpperCase (str) {
+    var result = ''
+
+    for (i = 0; i < str.length; i++) {
+      if (str[i] == str[i].toUpperCase()) {
+        result += str[i].toLowerCase()
+      } else if (str[i] == str[i].toLowerCase()) {
+        result += str[i].toUpperCase()
+      }
+    }
+  return result
+}
+
+function removeSpaces (str) {
+  var result = ''
+  
+  for (i = 0; i < str.length; i++) {
+    if (str[i] == ' ') {
+      str[i] = ''
+    } else {
+      result += str[i]
+    }
+  }
+ return result 
+}
+  
+function passwordGenerator (name) {
+  if (name.length < 5) {
+    return 'Minimal karakter yang diinputkan adalah 5 karakter'
+  }
+  
+  var vocals = changeVocals(name)
+  var reverse = reverseWord(vocals)
+  var lowerUpper = setLowerUpperCase(reverse)
+  var spaces = removeSpaces(lowerUpper)
+
+  return spaces
+}
+  
+  console.log(passwordGenerator('Sergei Dragunov')); // 'VPNVGBRdJFGRFs'
+  console.log(passwordGenerator('Dimitri Wahyudiputra')); // 'BRTVPJDVYHBwJRTJMJd'
+  console.log(passwordGenerator('Alexei')); // 'JFXFLb'
+  console.log(passwordGenerator('Alex')); // 'Minimal karakter yang diinputkan adalah 5 karakter'
