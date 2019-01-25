77609719452b38ead0ac5b956cb0fe8e5f61cec9
Reyhan Huditama <reyhan21098@gmail.com>
2019-01-25 13:32:27 +0700

    Tugas Week 5

---
+++ KaliTerusRekursif.js
@@ -0,0 +1,20 @@
+function kaliTerusRekursif(angka) {
+    var string = String(angka)
+
+    if (string.length === 1) {
+        return Number(angka)
+    } else {
+        var result = 1
+        for (i = 0; i < string.length; i++) {
+            result *= Number(string[i])
+        }
+    return kaliTerusRekursif(result)
+    }
+  }
+  
+  // TEST CASES
+  console.log(kaliTerusRekursif(66)); // 8
+  console.log(kaliTerusRekursif(3)); // 3
+  console.log(kaliTerusRekursif(24)); // 8
+  console.log(kaliTerusRekursif(654)); // 0
+  console.log(kaliTerusRekursif(1231)); // 6
